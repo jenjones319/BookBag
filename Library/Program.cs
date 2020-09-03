3b1a37c9d18f2d3f06005c639609c52c61bb1821
Jennifer B Jones <66266330+jenjones319@users.noreply.github.com>
2020-09-03 11:28:18 -0500

    added dependencies

--- Library/Program.cs
+++ Library/Program.cs
@@ -4,9 +4,39 @@ namespace Library
 {
     class Program
     {
+        public static Library<Book> Library { get; set; }
+        public static Library<Book> Library { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Library = new Library<Book>();
+            BookBag = new Library<Book>();
+
+            Console.WriteLine("Welcome to Dolly Parton's Imagination Library");
+            Console.WriteLine("Enter the letter for the command you need");
+            string input = Console.ReadLine();
+
+            if (input == "L") ;
+            LoadBooks();
         }
+
+        public static void AddABook(string title, string firstName, string lastName, int numberOfPages, Genre genre)
+        {
+            Book book = new Book()
+            {
+                Title = title,
+                Author = new Author()
+                {
+                    FirstName = firstName,
+                    LastName = lastName
+                },
+                NumberOfPages = numberOfPages,
+                Genre = genre
+            };
+
+            Library.Add(book);
+        }
+
     }
+}
 }
